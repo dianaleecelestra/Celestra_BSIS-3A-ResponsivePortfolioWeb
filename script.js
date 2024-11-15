dd6df5320aa00654f081881b40e516fbfd99e847
yananiXP <dianalee030@gmail.com>
2024-11-16 00:18:31 +0800

    fixing dis tmrw

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-to");
+      const icon = document.querySelector(".burger-icon");
+      menu.classList.toggle("open");
+      icon.classList.toggle("open"); 
+  }
